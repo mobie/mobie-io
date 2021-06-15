88db8f2cb486bdd2b1125d890f8c06c231101b9a
Морева Екатерина Ивановна <moreva.ei@edu.spbstu.ru>
2021-06-15 09:42:25 +0300

    Possible fix for Mac for tests OpenOMEZarrS3 and OpenOMEZarrS3XML

--- src/test/java/i2k2020/OpenBDVN5S3XML.java
+++ src/test/java/i2k2020/OpenBDVN5S3XML.java
@@ -5,13 +5,11 @@ import mpicbg.spim.data.SpimData;
 import mpicbg.spim.data.SpimDataException;
 import mpicbg.spim.data.XmlIoSpimData;
 
-public class OpenOMEZarrS3
+public class OpenBDVN5S3XML
 {
 	public static void main( String[] args ) throws SpimDataException
 	{
-		// Note: this requires the native blosc library
-		// Tischi: for me on my Mac it worked copying libblosc.dylib from Fiji into /src/main/resources
-		SpimData spimData = new XmlIoSpimData().load( "src/main/test/resources/prospr-myosin-zarr.xml" );
+		SpimData spimData = new XmlIoSpimData().load( "src/test/resources/prospr-myosin-n5.xml" );
 		BdvFunctions.show( spimData );
 	}
 }
--- src/test/java/i2k2020/OpenOMEZarrS3.java
+++ src/test/java/i2k2020/OpenOMEZarrS3.java
@@ -5,13 +5,13 @@ import mpicbg.spim.data.SpimData;
 import mpicbg.spim.data.SpimDataException;
 import mpicbg.spim.data.XmlIoSpimData;
 
-public class OpenOMEZarrS3XML
+public class OpenOMEZarrS3
 {
 	public static void main( String[] args ) throws SpimDataException
 	{
 		// Note: this requires the native blosc library
 		// Tischi: for me on my Mac it worked copying libblosc.dylib from Fiji into /src/main/resources
-		SpimData spimData = new XmlIoSpimData().load( "src/main/test/resources/prospr-myosin-zarr.xml" );
+		SpimData spimData = new XmlIoSpimData().load( "src/test/resources/prospr-myosin-zarr.xml" );
 		BdvFunctions.show( spimData );
 	}
 }
