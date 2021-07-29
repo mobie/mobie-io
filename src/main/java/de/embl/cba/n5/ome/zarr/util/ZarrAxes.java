8540b538a7cfad85edb387cb816c2e7010cb4e01
Морева Екатерина Ивановна <moreva.ei@edu.spbstu.ru>
2021-07-29 23:51:58 +0300

    ZarrAxes for ome.zarr v3 fix

--- src/main/java/de/embl/cba/n5/ome/zarr/util/ZarrAxes.java
+++ src/main/java/de/embl/cba/n5/ome/zarr/util/ZarrAxes.java
@@ -14,16 +14,43 @@ public enum ZarrAxes {
     CZYX("[\"c\",\"z\",\"y\",\"x\"]"),
     TZYX("[\"t\",\"z\",\"y\",\"x\"]"),
     TCYX("[\"t\",\"c\",\"y\",\"x\"]"),
-    TCZYX("[\"t\",\"c\",\"z\",\"y\",\"x\"]");
+    TCZYX("[\"t\",\"c\",\"z\",\"y\",\"x\"]"),
+
+    NOT_SPECIFIED("");
 
     private final String axes;
 
     @JsonCreator
     public static ZarrAxes decode(final String axes) {
-        return Stream.of(ZarrAxes.values()).filter(targetEnum -> targetEnum.axes.equals(axes)).findFirst().orElse(null);
+        return Stream.of(ZarrAxes.values()).filter(targetEnum ->
+                targetEnum.axes.equals(axes)).findFirst().orElse(NOT_SPECIFIED);
     }
 
     ZarrAxes(String axes) {
         this.axes = axes;
+    }
+
+    public boolean is2D() {
+        return this.axes.equals(YX.axes);
+    }
+
+    public boolean is5D() {
+        return this.axes.equals(TZYX.axes);
+    }
+
+    public boolean is4D() {
+        return this.axes.equals(CZYX.axes) || this.axes.equals(TZYX.axes) || this.axes.equals(TCYX.axes);
+    }
+
+    public boolean is4DWithTimepoints(){
+        return this.axes.equals(TZYX.axes);
+    }
+
+    public boolean is4DWithChannels() {
+        return this.axes.equals(CZYX.axes);
+    }
+
+    public boolean is4DWithTimepointsAndChannels() {
+        return this.axes.equals(TCYX.axes);
     }
 }
