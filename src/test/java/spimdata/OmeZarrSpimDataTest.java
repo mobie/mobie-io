e5f974b7d0aeb5afe0c33f056a95ae08ad5e58c2
Морева Екатерина Ивановна <moreva.ei@edu.spbstu.ru>
2021-08-19 01:37:24 +0300

    First SpimDataTest

---
+++ src/test/java/spimdata/OmeZarrSpimDataTest.java
@@ -0,0 +1,26 @@
+package spimdata;
+
+import de.embl.cba.n5.ome.zarr.openers.OMEZarrOpener;
+import mpicbg.spim.data.SpimData;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class OmeZarrSpimDataTest {
+    private static final String MAGICAL_PIXEL_VALUE = "131";
+
+    @Test
+    public void SpimDataTest() {
+        try {
+            SpimData spimData = OMEZarrOpener.openFile("/home/katerina/Documents/data/v0.3/zyx.ome.zarr");
+            Object realPixelValue = spimData.getSequenceDescription().getImgLoader().getSetupImgLoader(0).getImage(0).getAt(225, 129, 301);
+            assertEquals(realPixelValue.toString(), MAGICAL_PIXEL_VALUE);
+        } catch (IOException e) {
+            fail("SpimData loading error: " + e.getMessage());
+        }
+    }
+}
