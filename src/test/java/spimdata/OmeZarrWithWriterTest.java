8895647fb237bdcd821fa3196c899ec38c5010ed
Морева Екатерина Ивановна <moreva.ei@edu.spbstu.ru>
2021-08-31 14:32:29 +0300

    OmeZarr and ImgPlusToN5Writers

--- src/test/java/spimdata/OmeZarrWithWriterTest.java
+++ src/test/java/spimdata/OmeZarrWithWriterTest.java
@@ -1,4 +1,19 @@
 package spimdata;
 
+import de.embl.cba.n5.ome.zarr.projectcreator.DownsampleBlock;
+import de.embl.cba.n5.ome.zarr.projectcreator.WriteImgPlusToN5BdvOmeZarr;
+import de.embl.cba.n5.ome.zarr.projectcreator.WriteImgPlusToN5OmeZarr;
+import ij.IJ;
+import ij.ImagePlus;
+import org.janelia.saalfeldlab.n5.GzipCompression;
+
 public class OmeZarrWithWriterTest {
+    public static void main( String[] args ) {
+        ImagePlus imp = IJ.openImage("src/test/resources/ImgForTest.tif");
+        new WriteImgPlusToN5OmeZarr().export(imp, "src/test/resources/zyx.ome.zarr",
+                DownsampleBlock.DownsamplingMethod.Average, new GzipCompression());
+
+        new WriteImgPlusToN5BdvOmeZarr().export(imp, "src/test/resources/writingTest.xml",
+                DownsampleBlock.DownsamplingMethod.Average, new GzipCompression());
+    }
 }
