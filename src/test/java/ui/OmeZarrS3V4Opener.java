50f96ff810323aef08bc4ebe214fcd6c0e313b6c
Морева Екатерина Ивановна <moreva.ei@edu.spbstu.ru>
2022-01-26 10:41:28 +0300

    fix S3 test

--- src/test/java/ui/OmeZarrS3V4Opener.java
+++ src/test/java/ui/OmeZarrS3V4Opener.java
@@ -9,11 +9,31 @@ import java.io.IOException;
 
 public class OmeZarrS3V4Opener {
     public static void main(String[] args) throws IOException {
-        showV4();
+        showYX();
     }
 
-    public static void showV4() throws IOException {
+    public static void showYX() throws IOException {
         SpimData image = OMEZarrS3Opener.readURL("https://s3.embl.de/i2k-2020/ngff-example-data/v0.4/yx.ome.zarr");
+        BdvFunctions.show(image);
+    }
+    public static void showZYX() throws IOException {
+        SpimData image = OMEZarrS3Opener.readURL("https://s3.embl.de/i2k-2020/ngff-example-data/v0.4/zyx.ome.zarr");
+        BdvFunctions.show(image);
+    }
+    public static void showCYX() throws IOException {
+        SpimData image = OMEZarrS3Opener.readURL("https://s3.embl.de/i2k-2020/ngff-example-data/v0.4/cyx.ome.zarr");
+        BdvFunctions.show(image);
+    }
+    public static void showTYX() throws IOException {
+        SpimData image = OMEZarrS3Opener.readURL("https://s3.embl.de/i2k-2020/ngff-example-data/v0.4/tyx.ome.zarr");
+        BdvFunctions.show(image);
+    }
+    public static void showTCYX() throws IOException {
+        SpimData image = OMEZarrS3Opener.readURL("https://s3.embl.de/i2k-2020/ngff-example-data/v0.4/tcyx.ome.zarr");
+        BdvFunctions.show(image);
+    }
+    public static void showTCZYX() throws IOException {
+        SpimData image = OMEZarrS3Opener.readURL("https://s3.embl.de/i2k-2020/ngff-example-data/v0.4/tczyx.ome.zarr");
         BdvFunctions.show(image);
     }
 }
