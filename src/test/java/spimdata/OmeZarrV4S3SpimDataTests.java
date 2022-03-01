bf96cd623be6b52bcf8dc5eda7d60cc4943539b1
Kimberly Meechan <kimberly.meechan@embl.de>
2022-03-01 10:55:53 +0000

    add test for unit

---
+++ src/test/java/spimdata/OmeZarrV4S3SpimDataTests.java
@@ -0,0 +1,24 @@
+package spimdata;
+
+import mpicbg.spim.data.SpimData;
+import org.embl.mobie.io.ome.zarr.openers.OMEZarrS3Opener;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class OmeZarrV4S3SpimDataTests {
+    public static final String ZYX_FILE_KEY = "https://s3.embl.de/i2k-2020/ngff-example-data/v0.4/zyx.ome.zarr";
+
+    @Test
+    public void SpimDataV4UnitTest() throws IOException {
+        SpimData spimData = OMEZarrS3Opener.readURL(ZYX_FILE_KEY);
+
+        final String unit = spimData.getSequenceDescription().getViewSetupsOrdered().get( 0 ).getVoxelSize().unit();
+        final double[] voxelSize = spimData.getSequenceDescription().getViewSetupsOrdered().get( 0 ).getVoxelSize().dimensionsAsDoubleArray();
+
+        assertEquals("nanometer", unit);
+        assertArrayEquals( voxelSize, new double[]{64.0, 64.0, 64.0});
+    }
+}
