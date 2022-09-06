efcfbff7957d62b7a54ab4c9c70dc8f3998ef46c
ekaterina.moreva <ekaterina.moreva@vk.team>
2022-09-06 17:57:02 +0300

    Split Ngff tests

---
+++ src/test/java/projects/ngff/MultiImageNgffDataTest.java
@@ -0,0 +1,46 @@
+package projects.ngff;
+
+import org.embl.mobie.io.ImageDataFormat;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import bdv.img.cache.VolatileCachedCellImg;
+import lombok.extern.slf4j.Slf4j;
+import mpicbg.spim.data.SpimDataException;
+import net.imglib2.FinalDimensions;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.img.cell.CellGrid;
+import projects.remote.BaseTest;
+
+@Slf4j
+public class MultiImageNgffDataTest extends BaseTest {
+    private static final String URL = "https://s3.embl.de/i2k-2020/ngff-example-data/v0.4/multi-image.ome.zarr";
+    private static final ImageDataFormat FORMAT = ImageDataFormat.OmeZarrS3;
+
+    public MultiImageNgffDataTest() throws SpimDataException {
+        super(URL, FORMAT);
+        //set values for base test
+        setExpectedTimePoints(1);
+        setExpectedShape(new FinalDimensions(1024, 930));
+        setExpectedDType("uint16");
+    }
+
+    @Test
+    public void checkDataset() {
+        long x = 1;
+        long y = 1;
+        long z = 1;
+        long[] imageDimensions = spimData.getSequenceDescription().getImgLoader().getSetupImgLoader(0).getImage(0).dimensionsAsLongArray();
+        if (x > imageDimensions[0] || y > imageDimensions[1] || z > imageDimensions[2]) {
+            throw new RuntimeException("Coordinates out of bounds");
+        }
+
+        RandomAccessibleInterval<?> realPixelValue = spimData.getSequenceDescription().getImgLoader().getSetupImgLoader(0).getImage(0);
+        VolatileCachedCellImg volatileCachedCellImg = (VolatileCachedCellImg) realPixelValue;
+        CellGrid cellGrid = volatileCachedCellImg.getCellGrid();
+        long[] dims = new long[]{1024, 930, 1};
+        int[] cellDims = new int[]{256, 256, 1};
+        CellGrid expected = new CellGrid(dims, cellDims);
+        Assertions.assertEquals(expected, cellGrid);
+    }
+}
--- src/test/java/projects/ngff/YXNgffDataTest.java
+++ src/test/java/projects/ngff/YXNgffDataTest.java
@@ -1,32 +1,4 @@
-/*-
- * #%L
- * Fiji viewer for MoBIE projects
- * %%
- * Copyright (C) 2018 - 2022 EMBL
- * %%
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- * #L%
- */
-package projects.remote;
+package projects.ngff;
 
 import org.embl.mobie.io.ImageDataFormat;
 import org.junit.jupiter.api.Assertions;
@@ -38,13 +10,14 @@ import mpicbg.spim.data.SpimDataException;
 import net.imglib2.FinalDimensions;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.img.cell.CellGrid;
+import projects.remote.BaseTest;
 
 @Slf4j
-public class NgffExampleDataTest extends BaseTest {
+public class YXNgffDataTest extends BaseTest {
     private static final String URL = "https://s3.embl.de/i2k-2020/ngff-example-data/v0.4/yx.ome.zarr";
     private static final ImageDataFormat FORMAT = ImageDataFormat.OmeZarrS3;
 
-    public NgffExampleDataTest() throws SpimDataException {
+    public YXNgffDataTest() throws SpimDataException {
         super(URL, FORMAT);
         //set values for base test
         setExpectedTimePoints(1);
