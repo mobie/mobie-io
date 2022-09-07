1bfe17a79757d91bd71a9496289b3e45404fcaf9
Constantin Pape <c.pape@gmx.net>
2022-09-08 00:12:50 +0200

    Add ngff v03 tests

--- src/test/java/projects/ngff/v03/CYXNgffV03Test.java
+++ src/test/java/projects/ngff/v03/CYXNgffV03Test.java
@@ -7,7 +7,6 @@ import projects.ngff.base.CYXNgffBaseTest;
 @Slf4j
 public class CYXNgffV03Test extends CYXNgffBaseTest{
     private static final String URL = "https://s3.embl.de/i2k-2020/ngff-example-data/v0.3/cyx.ome.zarr";
-    
     public CYXNgffV03Test() throws SpimDataException {
         super(URL);
     }
