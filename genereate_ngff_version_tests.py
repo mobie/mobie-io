3636fe99763d1d5d3dba4e55e71ea2cbc80e7f06
Constantin Pape <c.pape@gmx.net>
2022-09-08 00:10:44 +0200

    Add script to generate ngff version tests and v04 tests

---
+++ genereate_ngff_version_tests.py
@@ -0,0 +1,44 @@
+import os
+import sys
+
+
+def generate_ngff_version_tests(version):
+
+    folder = f"./src/test/java/projects/ngff/v0{version}"
+    os.makedirs(folder, exist_ok=True)
+
+    test_names = {
+        "CYX": "cyx",
+        "CZYX": "czyx",
+        "MultiImage": "multi-image",
+        "TCYX": "tcyx",
+        "TCZYX": "tczyx",
+        "TYX": "tyx",
+        "YX": "yx",
+        "ZYX": "zyx",
+    }
+
+    for name, url_name in test_names.items():
+        if name == "MultiImage" and version < 4:
+            continue
+
+        test = f"""package projects.ngff.v0{version};
+
+import lombok.extern.slf4j.Slf4j;
+import mpicbg.spim.data.SpimDataException;
+import projects.ngff.base.{name}NgffBaseTest;
+
+@Slf4j
+public class {name}NgffV0{version}Test extends {name}NgffBaseTest{{
+    private static final String URL = "https://s3.embl.de/i2k-2020/ngff-example-data/v0.{version}/{url_name}.ome.zarr";
+    public {name}NgffV0{version}Test() throws SpimDataException {{
+        super(URL);
+    }}
+}}"""
+        test_file = os.path.join(folder, f"{name}NgffV0{version}Test.java")
+        with open(test_file, "w") as f:
+            f.write(test)
+
+
+if __name__ == "__main__":
+    generate_ngff_version_tests(int(sys.argv[1]))
---
+++ src/test/java/projects/ngff/v04/TCYXNgffV04Test.java
@@ -0,0 +1,13 @@
+package projects.ngff.v04;
+
+import lombok.extern.slf4j.Slf4j;
+import mpicbg.spim.data.SpimDataException;
+import projects.ngff.base.TCYXNgffBaseTest;
+
+@Slf4j
+public class TCYXNgffV04Test extends TCYXNgffBaseTest{
+    private static final String URL = "https://s3.embl.de/i2k-2020/ngff-example-data/v0.4/tcyx.ome.zarr";
+    public TCYXNgffV04Test() throws SpimDataException {
+        super(URL);
+    }
+}
